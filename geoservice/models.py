230beb41412db2ac1089637322eee9818a41d43c
Yury Blagoveschenskiy <yurathestorm@gmail.com>
2018-10-26 13:50:39 +0300

    Place model

--- geoservice/models.py
+++ geoservice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+
 
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='geoservice/photos/places')
+    lon = gis_models.FloatField()
+    lat = gis_models.FloatField()
+
+    def __str__(self):
+        return self.name
