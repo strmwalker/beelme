7d7ef50a93c86f27432d35961943efc3e5a39957
Yury Blagoveschenskiy <yurathestorm@gmail.com>
2018-10-28 12:58:37 +0300

    GeoDjango setup

--- geoservice/models.py
+++ geoservice/models.py
@@ -7,8 +7,7 @@ from django.contrib.gis.db import models as gis_models
 class Place(models.Model):
     name = models.CharField(max_length=50)
     photo = models.ImageField(upload_to='geoservice/photos/places')
-    lon = gis_models.FloatField()
-    lat = gis_models.FloatField()
+    location = gis_models.PointField()
 
     def __str__(self):
         return self.name
